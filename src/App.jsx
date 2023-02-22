e872ece49e4b60317f022b9839fe44913ef0bda9
Jeremias <blackarrowsb@gmail.com>
2023-02-21 23:53:19 -0300

    segunda pre entrega

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,27 @@
-import Destacado from "./components/Destacado";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer";
 import ItemListContainer from "./components/ItemListContainer";
+import ItemDetailContainer from "./components/ItemDetailContainer";
 import NavBar from "./components/NavBar";
-import PromoAsus from "./components/PromoAsus";
+import PromoApp from "./components/PromoApp";
+import Error404 from "./components/Error404";
 
 function App() {
   return (
-    <div>
-      <NavBar />  
-      <Destacado />
-      <ItemListContainer greeting={"Redefine the way to create"} />
-      <PromoAsus />   
-      <Footer /> 
-    </div>
-  )
-
-  
+    <BrowserRouter>
+      <div>
+        <NavBar />
+        <Routes>
+          <Route path={"/"} element={<ItemListContainer />} />
+          <Route path={"/category/:id"} element={<ItemListContainer />} />
+          <Route path={"/item/:id"} element={<ItemDetailContainer />} />
+          <Route path={"*"} element={<Error404 />} />
+        </Routes>
+        <PromoApp />
+        <Footer />
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,35 +1,38 @@
-import logo from "./images/asus_logo.png"
-import React from 'react'
 import CartWidget from "./CartWidget";
+import { Link, NavLink } from "react-router-dom";
+import logo from "./images/mcdonalds-logo-footer-bg-white.png";
 
 const NavBar = () => {
     return (
-        <div className="container-fluid">
+        <div className="container">
             <div className="row">
                 <div className="col">
                     <nav className="navbar navbar-expand-lg">
                         <div className="container-fluid">
-                            <a className="navbar-brand" href={"/"}> <img src={logo} alt="logo asus azul" width={"90"}/> </a>
+                            <Link className="navbar-brand" to={"/"}><img src={logo} alt="McDonalds Argentina" width={"108"} /></Link>
                             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                                <span className="navbar-toggler-icon"></span>
+                            <span className="navbar-toggler-icon"></span>
                             </button>
                             <div className="collapse navbar-collapse" id="navbarNav">
                                 <ul className="navbar-nav">
                                     <li className="nav-item">
-                                        <a className="nav-link" href={"/Gaming"}>Gaming</a>
+                                        <NavLink className="nav-link" activeclassname={"active"} to={"/"}>Productos</NavLink>
                                     </li>
                                     <li className="nav-item">
-                                        <a className="nav-link" href={"/Design"}>Design</a>
+                                        <NavLink className="nav-link" activeclassname={"active"} to={"/category/hamburguesas"}>Hamburguesas</NavLink>
                                     </li>
                                     <li className="nav-item">
-                                        <a className="nav-link" href={"/Student"}>Student</a>
+                                        <NavLink className="nav-link" activeclassname={"active"} to={"/category/papas"}>Papas Fritas</NavLink>
+                                    </li>
+                                    <li className="nav-item">
+                                        <NavLink className="nav-link" activeclassname={"active"} to={"/category/postres"}>Postres</NavLink>
                                     </li>
                                 </ul>
                             </div>
                         </div>
                     </nav>
                 </div>
-                <div className="col d-flex align-items-center justify-content-end m-5">
+                <div className="col d-flex align-items-center justify-content-end">
                     <CartWidget />
                 </div>
             </div>
