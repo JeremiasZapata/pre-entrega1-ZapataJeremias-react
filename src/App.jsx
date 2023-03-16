a1d2969e125ee1da784d89b55a0cf5ceec083208
Jeremias <blackarrowsb@gmail.com>
2023-03-16 17:38:07 -0300

    nuevo cambio

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer";
 import ItemListContainer from "./components/ItemListContainer";
 import ItemDetailContainer from "./components/ItemDetailContainer";
 import NavBar from "./components/NavBar";
 import PromoAsus from "./components/PromoAsus";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Error404 from "./components/Error404";
 
 function App() {
